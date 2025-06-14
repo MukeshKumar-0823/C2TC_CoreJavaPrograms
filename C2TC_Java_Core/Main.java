cfde55c33308c88c8b67d61d32b4ec0bb4849ac3
Mukesh Kumar V <mukeshkumarv0823@gmail.com>
2025-06-14 15:09:33 +0530

    Add files via upload

---
+++ C2TC_Java_Core/Main.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class Main {
+	 public static void main(String[] args) {
+	        student s1 = new student();
+	        s1.set("Mukesh",20);
+	        System.out.println("Name: " + s1.getName());
+	        System.out.println("Age: " + s1.getAge());
+	    }
+
+}
