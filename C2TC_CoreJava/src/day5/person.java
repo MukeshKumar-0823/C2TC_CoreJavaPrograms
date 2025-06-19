60f6f23d7c6818866800de3e764f8896d9b4afb5
Mukesh Kumar V <mukeshkumarv0823@gmail.com>
2025-06-19 11:50:30 +0530

    OK

---
+++ C2TC_CoreJava/src/day5/person.java
@@ -0,0 +1,39 @@
+package day5;
+
+public class person {
+	private int id;
+	private String name;
+	private String city;
+	public  person() {
+		this.id=101;
+		this.name="mukesh";
+		this.city="chennai";
+	}
+	public person(int id,String name,String city) {
+		this.city=city;
+		this.id=id;
+		this.name=name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "person [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+}
